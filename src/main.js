455b1ff7bf61902356fd79c95b656da99d3a41ec
Shakil Ahmed <sakilhossain01969@gmail.com>
2025-07-16 00:24:15 +0600

    add: work section filtering feature added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,36 @@ import './style.css';
 
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
+const projectCategories = document.querySelector('.project-categories');
+const projectsGrid = document.querySelector('.projects-grid');
+const projectItems = document.querySelectorAll('.project-item');
+
 
 menuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
 });
+
+projectCategories.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const category = e.target.dataset.category;
+    // remove active 
+    const activeBtn = projectCategories.querySelector('.active');
+    activeBtn.classList.remove('active');
+    // add active 
+    e.target.classList.add('active');
+
+    projectItems.forEach((item) => {
+      console.log(item);
+      if (category === 'all' || item.classList.contains(category)) {
+        item.classList.remove('hidden');
+      } else {
+        item.classList.add('hidden');
+      }
+    });
+  }
+});
+
+// click on first category initial
+const firstCategoryBtn = projectCategories.querySelector('button');
+firstCategoryBtn.click();
+
